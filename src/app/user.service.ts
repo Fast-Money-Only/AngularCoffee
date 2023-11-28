fc73a9e6a58889a94576bea0733c64218921f627
pernillepersson <pernillekp@me.com>
2023-11-28 11:54:37 +0100

    User model added and service implemented

---
+++ src/app/user.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {UserModel} from "./users/User.Model";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private baseUrl :string = "http://localhost:5056/ape/User";
+
+  constructor(private http: HttpClient) { }
+
+  getUsers() : Observable<Object>{
+    return this.http.get(this.baseUrl + "/GetUsers");
+  }
+
+  getUser(id: string): Observable<Object>{
+    return this.http.get(this.baseUrl + "/getUser/" + id);
+  }
+
+  updateUser(id: string, user: UserModel): Observable<Object>{
+    return this.http.put(this.baseUrl + "/" + id, user);
+  }
+
+  createUser(user: UserModel): Observable<any>{
+    return this.http.post(this.baseUrl, user);
+  }
+
+  deleteUser(id: string) : Observable<Object>{
+    return this.http.delete(this.baseUrl + "/" + id);
+  }
+
+  getUserOrders(id: string): Observable<Object>{
+    return this.http.get(this.baseUrl + "/Get user orders/" + id);
+  }
+
+
+}
+

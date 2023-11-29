3ed05727e59addb969df67e3ec06c911fcdcfd7b
pernillepersson <pernillekp@me.com>
2023-11-29 11:11:18 +0100

    Post and orders implemented

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from "../services/product.service";
+import {OrderService} from "../services/order.service";
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  data: any;
+
+
+  constructor(private service: OrderService) {
+  }
+
+  ngOnInit(): void {
+    this.service.getOrders().subscribe(data => this.data = data);
+    console.log(this.data);
+  }
 
 }
